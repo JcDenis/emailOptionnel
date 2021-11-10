dd582edee8ea1786f36d8e044f518d0a8ba2c0eb
Pierre <brol31@gmail.com>
2021-11-11 00:20:41 +0100

    Update _prepend.php

--- _prepend.php
+++ _prepend.php
@@ -2,7 +2,7 @@
 # -- BEGIN LICENSE BLOCK ----------------------------------
 # This file is part of Email Optionnel, a plugin for Dotclear.
 #
-# Copyright (c) 2007-2015 Oleksandr Syenchuk, Pierre Van Glabeke
+# Copyright (c) 2007-2020 Oleksandr Syenchuk, Pierre Van Glabeke, Gvx
 #
 # Licensed under the GPL version 2.0 license.
 # A copy is available in LICENSE file or at
