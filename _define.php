ae29c8066a63376b5fea383258b4a6958f3e12ca
Pierre <brol31@gmail.com>
2021-11-11 00:20:02 +0100

    Update _define.php

--- _define.php
+++ _define.php
@@ -2,7 +2,7 @@
 # -- BEGIN LICENSE BLOCK ----------------------------------
 # This file is part of Email Optionnel, a plugin for Dotclear.
 #
-# Copyright (c) 2007-2015 Oleksandr Syenchuk, Pierre Van Glabeke
+# Copyright (c) 2007-2020 Oleksandr Syenchuk, Pierre Van Glabeke, Gvx
 #
 # Licensed under the GPL version 2.0 license.
 # A copy is available in LICENSE file or at
@@ -19,14 +19,21 @@ if (!defined('DC_RC_PATH')) { return; }
 $this->registerModule(
 	/* Name */		'emailOptionnel',
 	/* Description*/	'Make e-mail address optional in comments',
-	/* Author */		'Oleksandr Syenchuk, Pierre Van Glabeke',
-	/* Version */		'0.4.6',
+	/* Author */		'Oleksandr Syenchuk, Pierre Van Glabeke, Gvx',
+	/* Version */		'0.5.0',
 	/* Properties */
 	array(
 		'permissions' => 'admin',
 		'type' => 'plugin',
-		'dc_min' => '2.6',
 		'support' => 'http://forum.dotclear.org/viewtopic.php?pid=332948#p332948',
-		'details' => 'http://plugins.dotaddict.org/dc2/details/emailOptionnel'
+		'details' => 'http://plugins.dotaddict.org/dc2/details/emailOptionnel',
+		'requires'	/* id(s) */	=>	array(
+			array('core', '2.11')
+		),
+		/* depuis dc 2.11 */
+		'settings'	=> array(
+			'blog'	=> '#params.emailOptionnelParam'
+		)
 	)
+
 );
