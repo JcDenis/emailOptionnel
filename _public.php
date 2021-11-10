3c5446a89903d19c96e895c2ceb13aa43bd8d57a
Pierre <brol31@gmail.com>
2021-11-11 00:21:13 +0100

    Update _public.php

--- _public.php
+++ _public.php
@@ -2,7 +2,7 @@
 # -- BEGIN LICENSE BLOCK ----------------------------------
 # This file is part of Email Optionnel, a plugin for Dotclear.
 #
-# Copyright (c) 2007-2015 Oleksandr Syenchuk, Pierre Van Glabeke
+# Copyright (c) 2007-2020 Oleksandr Syenchuk, Pierre Van Glabeke, Gvx
 #
 # Licensed under the GPL version 2.0 license.
 # A copy is available in LICENSE file or at
@@ -10,7 +10,6 @@
 # -- END LICENSE BLOCK ------------------------------------
 if (!defined('DC_RC_PATH')) { return; }
 
-$core->addBehavior('publicPrepend',
-	array('emailOptionnelBehaviors','publicPrepend'));
-$core->addBehavior('publicBeforeCommentCreate',
-	array('emailOptionnelBehaviors','publicBeforeCommentCreate'));
+$core->addBehavior('publicPrepend', array('emailOptionnelBehaviors','publicPrepend'));
+$core->addBehavior('publicBeforeCommentCreate', array('emailOptionnelBehaviors','publicBeforeCommentCreate'));
+$core->addBehavior('publicHeadContent', array('emailOptionnelBehaviors','publicHeadContent'));
