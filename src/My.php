95a3468b3b57e7f266cedc65c0ed184c59e8a4f9
Jean-Christian Denis <contact@jcdenis.fr>
2023-05-10 11:21:34 +0200

    no more needed

--- src/My.php
+++ src/My.php
@@ -21,9 +21,6 @@ use dcCore;
  */
 class My extends \initEmailOptionnel
 {
-    /** @var    string  This module required php version */
-    public const PHP_MIN = '7.4';
-
     /**
      * This module id.
      */
@@ -48,13 +45,5 @@ class My extends \initEmailOptionnel
     public static function path(): string
     {
         return dirname(__DIR__);
-    }
-
-    /**
-     * Check this module PHP version compliant.
-     */
-    public static function phpCompliant(): bool
-    {
-        return version_compare(phpversion(), self::PHP_MIN, '>=');
     }
 }
