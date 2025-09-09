54c8c93488c9fefe85be5b26d6a1df01b706a6c8
Jean-Christian Denis <contact@jcdenis.fr>
2025-09-09 18:18:24 +0200

    release 1.6.1

--- _define.php
+++ _define.php
@@ -18,7 +18,7 @@ $this->registerModule(
     'emailOptionnel',
     'Make e-mail address optional in comments',
     'Oleksandr Syenchuk, Pierre Van Glabeke, Gvx and Contributors',
-    '1.6',
+    '1.6.1',
     [
         'requires'    => [['core', '2.36']],
         'permissions' => 'My',
